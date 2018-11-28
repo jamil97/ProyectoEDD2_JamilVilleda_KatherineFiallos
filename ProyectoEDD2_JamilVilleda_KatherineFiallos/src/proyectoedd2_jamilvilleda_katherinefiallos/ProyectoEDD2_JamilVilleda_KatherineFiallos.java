21cd71ea3d9f7d778999c2b447b86cea42ec26f0
jamil97 <jamvilleda97@live.com>
2018-11-28 09:23:17 -0600

    Inicio del proyecto

---
+++ ProyectoEDD2_JamilVilleda_KatherineFiallos/src/proyectoedd2_jamilvilleda_katherinefiallos/ProyectoEDD2_JamilVilleda_KatherineFiallos.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoedd2_jamilvilleda_katherinefiallos;
+
+/**
+ *
+ * @author Jamil
+ */
+public class ProyectoEDD2_JamilVilleda_KatherineFiallos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ ProyectoEDD2_JamilVilleda_KatherineFiallos/src/proyectoedd2_jamilvilleda_katherinefiallos/Registro.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoedd2_jamilvilleda_katherinefiallos;
+
+/**
+ *
+ * @author Jamil
+ */
+public class Registro {
+    
+}
