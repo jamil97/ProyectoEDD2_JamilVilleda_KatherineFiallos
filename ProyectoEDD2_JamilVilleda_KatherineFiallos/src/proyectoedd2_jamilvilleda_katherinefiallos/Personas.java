c246829bef32ae919a6f2fe204ce7dddf29214dc
kathe_mantunez <kathe_mantunez@unitec.edu>
2018-12-15 22:52:12 -0600

    hola

--- ProyectoEDD2_JamilVilleda_KatherineFiallos/src/proyectoedd2_jamilvilleda_katherinefiallos/Personas.java
+++ ProyectoEDD2_JamilVilleda_KatherineFiallos/src/proyectoedd2_jamilvilleda_katherinefiallos/Personas.java
@@ -10,5 +10,112 @@ package proyectoedd2_jamilvilleda_katherinefiallos;
  * @author KATHERINE
  */
 public class Personas {
+    private int id;
+    private String name;
+    private String birthdate;
+    private String place;
+    private String name_father;
+    private String name_mother;
+    
+    private char borrado;
+    private int referencia;
+
+    public Personas(int id, String name, String birthdate, String place, String name_father, String name_mother, char borrado, int referencia) {
+        this.id = id;
+        this.name = name;
+        this.birthdate = birthdate;
+        this.place = place;
+        this.name_father = name_father;
+        this.name_mother = name_mother;
+        this.borrado = '-';
+        this.referencia = 0;
+    }
+
+    public Personas() {
+    }
+    
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        for (int i =name.length(); i < 40; i++) {
+            name+=" ";
+            
+        }
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+    public String getName_father() {
+        return name_father;
+    }
+
+    public void setName_father(String name_father) {
+        this.name_father = name_father;
+    }
+
+    public String getName_mother() {
+        return name_mother;
+    }
+
+    public void setName_mother(String name_mother) {
+        this.name_mother = name_mother;
+    }
+
+    public char getBorrado() {
+        return borrado;
+    }
+
+    public void setBorrado(char borrado) {
+        this.borrado = borrado;
+    }
+
+    public int getReferencia() {
+        return referencia;
+    }
+
+    public void setReferencia(int referencia) {
+        this.referencia = referencia;
+    }
+    public int sizeOf(){
+        int size=68;
+        return size;
+    }
+
+    @Override
+    public String toString() {
+        return "Personas{" + "id=" + id + ", name=" + name + ", birthdate=" + birthdate + ", place=" + place + ", name_father=" + name_father + ", name_mother=" + name_mother + '}';
+    }
+
+    
+    
+    
+    
+       
     
 }
--- ProyectoEDD2_JamilVilleda_KatherineFiallos/src/proyectoedd2_jamilvilleda_katherinefiallos/TDA_ArchivoFijo.java
+++ ProyectoEDD2_JamilVilleda_KatherineFiallos/src/proyectoedd2_jamilvilleda_katherinefiallos/TDA_ArchivoFijo.java
@@ -5,18 +5,13 @@
  */
 package proyectoedd2_jamilvilleda_katherinefiallos;
 
-import com.sun.java.util.jar.pack.ConstantPool.Index;
-import java.io.EOFException;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.io.RandomAccessFile;
 import java.util.LinkedList;
 import javax.swing.table.DefaultTableModel;
+
+
 
 /**
  *
@@ -37,7 +32,7 @@ public class TDA_ArchivoFijo {
         }
         flujo=new RandomAccessFile(file, "rw");
         list=new LinkedList();
-        arbol=new Btree();
+        arbol=new BTree();
         flujo.seek(0);
         try {
             if(file.length()>0){
@@ -63,7 +58,174 @@ public class TDA_ArchivoFijo {
     }
     public boolean insert(Personas personas){
         boolean created=false;
-        indice posicion=new indice());
+        Index posicion=new Index();
+        posicion=arbol.search(personas.getId());
+        if(posicion!=null){
+            return false;
+        }else{
+            try {
+                flujo.seek(0+headerSize);
+                while(true){
+                    if(list.isEmpty()){
+                        created=true;
+                        flujo.seek(file.length());
+                        flujo.writeChar(personas.getBorrado());
+                        flujo.writeInt(0);
+                        flujo.writeInt(personas.getId());
+                        flujo.writeUTF(personas.getName());
+                        flujo.writeUTF(personas.getBirthdate());
+                        flujo.writeUTF(personas.getPlace());
+                        flujo.writeUTF(personas.getName_father());
+                        flujo.writeUTF(personas.getName_mother());
+                        arbol.insert(new Index(personas.getId(),(int)file.length()/personas.sizeOf()));
+                        break;
+                    }else{
+                        flujo.seek(0);
+                        created=true;
+                        int pos=(int)list.remove(0);
+                        int ref=0;
+                        flujo.seek(personas.sizeOf()*(pos-1)+headerSize);
+                        flujo.readChar();
+                        ref=flujo.readInt();
+                        flujo.seek(0);
+                        flujo.writeInt(ref);
+                        flujo.seek(personas.sizeOf()*(pos-1)+headerSize);
+                        flujo.writeChar(personas.getBorrado());
+                        flujo.writeInt(0);
+                        flujo.writeInt(personas.getId());
+                        flujo.writeUTF(personas.getName());
+                        flujo.writeUTF(personas.getBirthdate());
+                        flujo.writeUTF(personas.getPlace());
+                        flujo.writeUTF(personas.getName_father());
+                        flujo.writeUTF(personas.getName_mother());
+                        arbol.insert(new Index(personas.getId(),pos));
+                        break;
+                    }
+                }
+                
+            } catch (Exception e) {
+            }
+        }
+        return created;  
+    }
+    
+    public boolean detele(int id) throws IOException{
+        Personas personas=new Personas();
+        boolean found=false;
+        Index rp=new Index();
+        rp =arbol.search(id);
+        try {
+            if(rp==null){
+                return found;
+            }else{
+                flujo.seek((rp.getRp()-1)*personas.sizeOf()+headerSize);
+                personas.setBorrado(flujo.readChar());
+                if(personas.getBorrado()!='*'){
+                    found=true;
+                    personas.setBorrado('*');
+                    flujo.seek(0);
+                    header=flujo.readInt();
+                    if(header==-1){
+                        flujo.seek(0);
+                        flujo.writeInt(rp.getRp());
+                        flujo.seek((rp.getRp()-1)* personas.sizeOf()+headerSize);
+                        flujo.writeChar(personas.getBorrado());
+                        flujo.writeInt(header);
+                        list.add(0,rp.getRp());
+                        arbol.delete(rp.getId());
+                    }else{
+                        flujo.seek(0);
+                        flujo.writeInt(rp.getRp());
+                        flujo.seek((rp.getRp()-1)* personas.sizeOf()+headerSize);
+                        flujo.writeChar(personas.getBorrado());
+                        flujo.writeInt(header);
+                        list.add(0,rp.getRp());
+                        arbol.delete(rp.getId());
+                    }
+                    return found;
+                }
+            }
+        } catch (Exception e) {
+        }
+        return found;
+    }
+    
+    public Personas search(int id)throws IOException{
+        Index pos=new Index();
+        pos=arbol.search(id);
+        Personas buscando=new Personas();
+        if(pos==null){
+            return null;
+        }else{
+            flujo.seek((pos.getRp()-1)*buscando.sizeOf()+headerSize);
+            buscando.setBorrado(flujo.readChar());
+            buscando.setReferencia(flujo.readInt());
+            buscando.setId(flujo.readInt());
+            buscando.setName(flujo.readUTF());
+            buscando.setBirthdate(flujo.readUTF());
+            buscando.setPlace(flujo.readUTF());
+            buscando.setName_father(flujo.readUTF());
+            buscando.setName_mother(flujo.readUTF());
+            return buscando;
+        }
+    }
+    public boolean modify(Personas newPersonas,int id)throws IOException{
+        Index pos=new Index();
+        pos=arbol.search(id);
+        System.out.println(pos.getId());
+        if(pos==null){
+            return false;
+        }else if(pos.getId() !=newPersonas.getId()){
+            flujo.seek((pos.getRp()-1)*newPersonas.sizeOf()+headerSize);
+            flujo.writeChar(newPersonas.getBorrado());
+            flujo.writeInt(newPersonas.getReferencia());
+            flujo.writeInt(newPersonas.getId());
+            flujo.writeUTF(newPersonas.getBirthdate());
+            flujo.writeUTF(newPersonas.getName());
+            flujo.writeUTF(newPersonas.getPlace());
+            flujo.writeUTF(newPersonas.getName_father());
+            flujo.writeUTF(newPersonas.getName_mother());
+            arbol.delete(id);
+            arbol.insert(pos);
+            return true;
+        }else{
+            flujo.seek((pos.getRp()-1)*newPersonas.sizeOf()+headerSize);
+            flujo.writeChar(newPersonas.getBorrado());
+            flujo.writeInt(newPersonas.getReferencia());
+            flujo.writeInt(newPersonas.getId());
+            flujo.writeUTF(newPersonas.getBirthdate());
+            flujo.writeUTF(newPersonas.getName());
+            flujo.writeUTF(newPersonas.getPlace());
+            flujo.writeUTF(newPersonas.getName_father());
+            flujo.writeUTF(newPersonas.getName_mother());
+            return true;
+        }
+    }
+    public DefaultTableModel listar(DefaultTableModel model){
+        try {
+            while(model.getRowCount()>0){
+                model.removeRow(0);
+            }
+            Personas personas=new Personas();
+            flujo.seek(0+headerSize);
+            while(true){
+                personas.setBorrado(flujo.readChar());
+                personas.setReferencia(flujo.readInt());
+                personas.setId(flujo.readInt());
+                personas.setName(flujo.readUTF());
+                personas.setPlace(flujo.readUTF());
+                personas.setBirthdate(flujo.readUTF());
+                personas.setName_father(flujo.readUTF());
+                personas.setName_mother(flujo.readUTF());
+                if(personas.getBorrado()!='*'){
+                    model.addRow(new Object[]{
+                        personas.getId(),personas.getName(),personas.getBirthdate(),personas.getPlace(),personas.getName_father(),personas.getName_mother()});
+                }
+            }
+            
+        } catch (Exception e) {
+        }
+        return model;
     }
     
     
